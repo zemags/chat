405e9946ab1fadc5cf000e7edd0581e4af31b186
zemags <azmt2008@gmail.com>
2020-11-21 12:10:06 -0800

    Add async consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,7 @@
 # consumers its like views in django, but asynchronously
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 # every consumer have connect, disconnect, recieve
 
 class ChatCunsumer(WebsocketConsumer):
@@ -17,5 +17,22 @@ class ChatCunsumer(WebsocketConsumer):
         json_data = json.loads(text_data)
         message = json_data['message']
         self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+
+class AsyncChatCunsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, code):
+        #  code - error code
+        pass
+
+    async def receive(self, text_data=None, bytes_data=None):
+        # receive and send back this message
+        json_data = json.loads(text_data)
+        message = json_data['message']
+        await self.send(text_data=json.dumps({
             'message': message
         }))
--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .consumers import ChatCunsumer
+from .consumers import ChatCunsumer, AsyncChatCunsumer
 
 #  list of urls for websocket work
 websocket_urls = [
     url(r'^ws/chat/$', ChatCunsumer.as_asgi()),  # url start with ws - websocket   as_asgi like as_view
+    url(r'^ws/achat/$', AsyncChatCunsumer.as_asgi()),
 ]
