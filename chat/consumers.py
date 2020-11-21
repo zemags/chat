7d38799bfeb7b813421c435744cf3437c944448c
zemags <azmt2008@gmail.com>
2020-11-21 12:35:40 -0800

    Add Async Json cosumers

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,14 +4,17 @@ import json
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 # every consumer have connect, disconnect, recieve
 
-from channels.consumer import SyncConsumer
+from channels.consumer import SyncConsumer, AsyncConsumer
+
+# coding and decoding cumsumver for asynchronously
+from channels.generic.websocket import JsonWebsocketConsumer, AsyncJsonWebsocketConsumer
 
 class ChatCunsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
     def disconnect(self, code):
-        #  code - error code
+        # code - error code
         pass
 
     def receive(self, text_data=None, bytes_data=None):
@@ -53,3 +56,58 @@ class BaseSyncConsumer(SyncConsumer):
             'type': 'websocket.send',
             'text': event['text']  # send back event with our text
         })
+
+
+class BaseAsyncConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        await self.send({
+            'type': 'websocket.accept'
+        })
+
+    async def websocket_receive(self, event):
+        await self.send({
+            'type': 'websocket.send',
+            'text': event['text']  # send back event with our text
+        })
+
+
+class ChatJsonCunsumer(JsonWebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, code):
+        # code - error code
+        pass
+
+    def receive_json(self, content, **kwargs):
+        # in content enconding\deconding json
+        self.send_json(content=content)  # send back users message
+
+    @classmethod  # redefine EncodeJson class logic
+    def encode_json(cls, content):
+        return super().encode_json(content)
+
+    @classmethod  # redefine DecodeJson classes methods
+    def decode_json(cls, content):
+        return super().decode_json(content)
+
+
+class ChatAsyncJsonCunsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, code):
+        # code - error code
+        pass
+
+    async def receive_json(self, content, **kwargs):
+        # in content enconding\deconding json
+        await self.send_json(content=content)  # send back users message
+
+    @classmethod  # redefine EncodeJson class logic
+    async def encode_json(cls, content):
+        return await super().encode_json(content)
+
+    @classmethod  # redefine DecodeJson classes methods
+    async def decode_json(cls, content):
+        return await super().decode_json(content)
--- chat/routing.py
+++ chat/routing.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
-from .consumers import ChatCunsumer, AsyncChatCunsumer, BaseSyncConsumer
+from .consumers import ChatCunsumer, AsyncChatCunsumer, BaseSyncConsumer, BaseAsyncConsumer, ChatJsonCunsumer, ChatAsyncJsonCunsumer
 
 #  list of urls for websocket work
 websocket_urls = [
     url(r'^ws/chat/$', ChatCunsumer.as_asgi()),  # url start with ws - websocket   as_asgi like as_view
     url(r'^ws/async_chat/$', AsyncChatCunsumer.as_asgi()),
     url(r'^ws/base_sync_chat/$', BaseSyncConsumer.as_asgi()),
+    url(r'^ws/base_async_chat/$', BaseAsyncConsumer.as_asgi()),
+    url(r'^ws/json_sync_chat/$', ChatJsonCunsumer.as_asgi()),
+    url(r'^ws/json_async_chat/$', ChatAsyncJsonCunsumer.as_asgi()),
 ]
